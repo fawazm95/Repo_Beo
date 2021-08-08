060d71a441b43f1a2658a7a544cdbec4d4458c77
fawazm95 <87188412+fawazm95@users.noreply.github.com>
2021-08-08 22:40:26 +0530

    Add files via upload
    Pused 2

---
+++ main.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+from itertools import product
+from itertools import repeat
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure ()
+ax = fig.gca (projection = '3d')
+points = product (* repeat ((-1, 1), 3))
+for p1, p2 in combinations (points, 2):
+    if [* map (sum, zip (p1, p2))]. count (0) == 1:
+       ax.plot3D (* zip (p1, p2), color = "b")
+plt.show ()
